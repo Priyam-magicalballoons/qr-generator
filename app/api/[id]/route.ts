748fe72287a2f031a0fb1ad88903bb34ed055927
Priyam-magicalballoons <magicalballoons.priyam@gmail.com>
2025-04-28 11:35:09 +0530

    initial commit

--- app/api/[id]/route.ts
+++ app/api/[id]/route.ts
@@ -4,15 +4,17 @@ import prisma from "@/lib/prisma";
 
 export async function GET(req: NextRequest) {
   const url = new URL(req.url);
+  console.log(url)
   const pathSegments = url.pathname.split("/");
   const id = pathSegments[pathSegments.length - 1];
+  console.log(id)
   const longUrl = await prisma.shortener.findFirst({
     where: {
       shortUrl: id,
     },
   });
-  const mapedLongUrl = getUrl(id);
-  if (longUrl || mapedLongUrl) {
+  // const mapedLongUrl = getUrl(id);
+  if (longUrl ) {
     await prisma.shortener.updateMany({
       where: {
         shortUrl: id,
@@ -23,9 +25,7 @@ export async function GET(req: NextRequest) {
         },
       },
     });
-    console.log("mapped long url",mapedLongUrl)
-    console.log(" long url",longUrl)
-    return NextResponse.redirect(mapedLongUrl ?? longUrl!.longUrl);
+    return NextResponse.redirect(longUrl!.longUrl);
   }
 
   return NextResponse.json({ error: "URL not found" }, { status: 404 });
