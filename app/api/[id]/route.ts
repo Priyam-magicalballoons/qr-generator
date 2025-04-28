df1adb290a9cad4cc1871739553e8e1056195114
Priyam-magicalballoons <magicalballoons.priyam@gmail.com>
2025-04-28 11:04:39 +0530

    initial commit

--- app/api/[id]/route.ts
+++ app/api/[id]/route.ts
@@ -1,27 +1,30 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { getUrl } from '@/lib/urlStore';
-import prisma from '@/lib/prisma';
+import { NextRequest, NextResponse } from "next/server";
+import { getUrl } from "@/lib/urlStore";
+import prisma from "@/lib/prisma";
 
-export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+export async function GET(req: NextRequest) {
+  const url = new URL(req.url);
+  const pathSegments = url.pathname.split("/");
+  const id = pathSegments[pathSegments.length - 1];
   const longUrl = await prisma.shortener.findFirst({
-    where : {
-      shortUrl : params.id
-    }
-  })
-  const mapedLongUrl = getUrl(params.id)
+    where: {
+      shortUrl: id,
+    },
+  });
+  const mapedLongUrl = getUrl(id);
   if (longUrl || mapedLongUrl) {
     await prisma.shortener.updateMany({
-      where : {
-        shortUrl : params.id
+      where: {
+        shortUrl: id,
       },
-      data : {
-        scannedCounts : {
-          increment : 1
-        }
-      }
-    })
+      data: {
+        scannedCounts: {
+          increment: 1,
+        },
+      },
+    });
     return NextResponse.redirect(mapedLongUrl ?? longUrl!.longUrl);
   }
 
-  return NextResponse.json({ error: 'URL not found' }, { status: 404 });
+  return NextResponse.json({ error: "URL not found" }, { status: 404 });
 }
