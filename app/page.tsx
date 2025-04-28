adca93302ea2ffc58d25f6e1b69ad75b0ec51eb4
Priyam-magicalballoons <magicalballoons.priyam@gmail.com>
2025-04-28 11:07:30 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -60,6 +60,30 @@ export default function HomePage() {
     fetchSheet();
   }, []);
 
+  // import { getUrl } from '@/lib/urlStore';
+// import QRCode from 'qrcode';
+// import { NextRequest, NextResponse } from 'next/server';
+
+// export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+//   const longUrl = getUrl(params.id);
+//   if (!longUrl) {
+//     return new NextResponse('Invalid QR ID', { status: 404 });
+//   }
+
+//   const qrUrl = `http://192.168.1.18:3000/api/${params.id}`;
+//   const qrDataUrl = await QRCode.toDataURL(qrUrl);
+//   const base64Data = qrDataUrl.split(',')[1];
+//   const imgBuffer = Buffer.from(base64Data, 'base64');
+
+//   return new NextResponse(imgBuffer, {
+//     headers: {
+//       'Content-Type': 'image/png',
+//       'Content-Length': imgBuffer.length.toString(),
+//     },
+//   });
+// }
+
+
   return (
     <main
       style={{ padding: "2rem", fontFamily: "Arial, sans-serif" }}
